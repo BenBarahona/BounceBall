c7297e019b00b85b34f36378b63c20e5ea56e163
Benjamin Barahona <bbaraona@gmail.com>
2016-03-09 20:05:35 -0600

    Moved borders to screen limit

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,12 +12,32 @@ public class GameController : MonoBehaviour {
 		spawner = GetComponent<BallSpawner> ();
 
 		if (spawner != null) {
-			
+			//Reasign spawner limits	
 		}
+
+		updateBorderObjectsPosition ();
+		Debug.Log (screenSize);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	private void updateBorderObjectsPosition()
+	{
+		//Don't know if top and bottom borders need to be moved... probably not
+		GameObject bottom = GameObject.Find ("/Borders/Bottom");
+		GameObject top = GameObject.Find ("/Borders/Top");
+		GameObject left = GameObject.Find ("/Borders/Left");
+		GameObject right = GameObject.Find ("/Borders/Right");
+
+		Vector3 position = left.transform.position;
+		position.x = -screenSize.x;
+		left.transform.position = position;
+
+		position = right.transform.position;
+		position.x = screenSize.x;
+		right.transform.position = position;
 	}
 }
